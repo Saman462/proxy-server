f3d2921f5bd9356b1d711de6df19dabc078917b0
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2024-09-13 20:59:18 +0500

    new

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use('/data', createProxyMiddleware({
         },
         error: (err, req, res) => {
             console.error('Proxy error:', err);
-            res.status(500).send('Proxy error');
+
         }
     },
 }));
